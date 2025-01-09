ab72d1ec4742eeb89beaa1939b014f0ae2885064
davidpu2004 <davidpu2004@gmail.com>
2025-01-09 18:08:47 -0500

    add save_as_file method
    add save_as_file method

--- AudioToText.py
+++ AudioToText.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 import time
+import os
 
 
 class AudioToText:
@@ -39,18 +40,24 @@ class AudioToText:
         result = result["text"]
         return result
 
+    def save_as_file(self, output_path, file_name, text):
+        output_file = os.path.join(output_path, file_name)
+        with open(output_file, 'w') as file:
+            file.write(text)
+
 
 def main():
     start_time = time.time()
     audio_to_text = AudioToText()
-    #the audio file ,from which we need to get transcript
+    # the audio file ,from which we need to get transcript
     audio_file = "speech.mp3"
     transcript = audio_to_text.get_transcript(audio_file)
     end_time = time.time()
     execution_time = end_time - start_time
     print("*****OpenAI Whisper Model***** \n\n")
     print(f"Processing audio to text takes time: {execution_time:.6f} seconds")
-    print("*****the audio transcript***** \n\n",transcript)
+    print("*****the audio transcript***** \n\n", transcript)
+    audio_to_text.save_as_file(".","speech.txt",transcript)
 
 
 if __name__ == "__main__":
